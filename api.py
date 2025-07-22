c5c76ae076332e77bcb0039c18c98bf205368d5d
Dmitrii Kokarev <dkokarev.91@gmail.com>
2025-07-22 20:07:33 +0200

    api.py Debug=False

--- api.py
+++ api.py
@@ -80,4 +80,4 @@ def health_check():
     return jsonify({'status': 'ok'})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001, debug=True) 
+    app.run(host='0.0.0.0', port=5001, debug=False) 
